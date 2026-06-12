7eed512a173a8a00e9dcd83a18f28f6567b37fb1
JEAN EUGENIO CAMENT VALDERRAMA <je.cament@duocuc.cl>
2026-06-12 14:15:43 -0400

    arreglo de error en ingesta

--- Scripts/pipeline_ingesta/ingesta.py
+++ Scripts/pipeline_ingesta/ingesta.py
@@ -61,7 +61,7 @@ def leer_archivo(direccion_archivo : str, sheet_excel = 0 ) -> pd.DataFrame:
 
         
 ##funcion que obtiene el dataset, lo guarda en bruto y devuelve la ruta del archivo 
-def guardar_archivo_bruto_url(url : str = "https://usc-excel.officeapps.live.com/x/_layouts/XlFileHandler.aspx?sheetName=in&downloadAsCsvEnabled=1&WacUserType=WOPI&usid=30e61928-581b-0527-9d70-a6c639959b9a&NoAuth=1&waccluster=PUS8") -> str:
+def guardar_archivo_bruto_url(url : str = "https://drive.usercontent.google.com/download?id=1qI7EOHSY-xMH5-05fRIPalH2grxDaYt9&export=download&authuser=0&confirm=t&uuid=2b0118bb-e452-4542-9160-786a02e477c4&at=AAINaIKZ0_qxa-SvTcmRwbhbvWon:1781288030280") -> str:
     
 
     os.makedirs("data/raw", exist_ok=True)
@@ -70,12 +70,12 @@ def guardar_archivo_bruto_url(url : str = "https://usc-excel.officeapps.live.com
 
     print(response)
 
-    with open("data/raw/viajes_transporte_raw.csv", "w", encoding="utf-8") as f:
-        f.write(response.text)       
+    with open("data/raw/viajes_transporte_raw.csv", "wb") as f:
+        f.write(response.content)       
 
     lgu.logging_ingesta.info("archivo viajes_transporte_raw.csv cargado en bruto en data/raw")
 
-
+guardar_archivo_bruto_url()
 
     
 
